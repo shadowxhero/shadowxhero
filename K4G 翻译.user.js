ec452cc1b6b5740dc755d5fd8945d6abc7e320f9
shadowxhero <zeroglh@gmail.com>
2026-03-11 15:29:15 +0800

    Update K4G 翻译.user.js

--- K4G 翻译.user.js
+++ K4G 翻译.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         K4G 本地词典翻译
 // @namespace    k4g-local-cn
-// @version      3.4.4
+// @version      3.4.5
 // @description  本地词典翻译；长词优先；英文大小写不敏感可开关；跳过商品标题/图片区；支持导入导出；支持动态“xx Keys/Amount: xx Keys”；补充Key/Keys复数短语；修复状态标签中文被截断；已内置用户词典；修复弹窗h1不翻译；修复分段中文空格；右下角仅在有替换时显示；修复CI下KEY/Key冲突；去重精简词典；新增通知弹窗动态翻译
 // @match        https://k4g.com/*
 // @match        https://www.k4g.com/*
@@ -464,6 +464,7 @@
     'Not logged in or token expired.': '未登录或登录凭证已过期。',
     'Active from / to': '有效期限：从 / 至',
     'Register': '注册',
+    'Question general': '一般问题',
     'FROM:': '来自：'
   };
 
